8141d27f4f6f42430fa2fe44230621f438165ebd
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-08-10 15:53:45 +0300

    chore(api): integrate server configuration and logger, load global configuration modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-
+import { awsConfig } from './config/aws.config';
+import { databaseConfig } from './config/database.config';
+import { searchConfig } from './config/search.config';
+import { serverConfig } from './config/server.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [serverConfig, databaseConfig, awsConfig, searchConfig],
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigType } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { serverConfig } from './config/server.config';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  const server = app.get<ConfigType<typeof serverConfig>>(serverConfig.KEY);
+
+  await app.listen(server.port);
+  Logger.log(`API listening on http://localhost:${server.port}`, 'Bootstrap');
 }
-bootstrap();
+
+void bootstrap();
